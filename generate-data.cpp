b72e783f0ab965bde463b26f37c7693b391cd7ad
tomekrzymyszkiewicz <tomek.rzymyszkiewicz@gmail.com>
2021-03-28 22:01:24 +0200

    generading array with random numbers

--- generate-data.cpp
+++ generate-data.cpp
@@ -1,6 +1,20 @@
 #include<stdio.h>
+#include<random>
+#include<ctime>
+#include<iostream>
 using namespace std;
 
+
+int* array_with_random_numbers(int size, int min,int max){
+    int* array = new int[size];
+    srand(time(NULL));
+    for(int i = 0; i < size; i++){
+        array[i] = (rand() % (2*max+1)) + min;
+    }
+    return array;
+}
+
 int main(){
+    
     return 0;
 }

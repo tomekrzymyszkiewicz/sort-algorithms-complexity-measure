18f212c6421f4fc23bf1eff3d809a515e9d5cdc4
tomekrzymyszkiewicz <tomek.rzymyszkiewicz@gmail.com>
2021-03-29 21:33:10 +0200

    loading new config syntax

--- complexity-measure.cpp
+++ complexity-measure.cpp
@@ -6,7 +6,12 @@
 #include<fstream>
 using namespace std;
 
+vector<int> data_vector;
 vector<vector<string>> tasks;
+vector<string> results;
+string data_file_name = "";
+int data_amount = 0;
+string results_file_name = "";
 
 void swap(int* a, int* b)
 {
@@ -42,13 +47,38 @@ void quick_sort(int arr[], int low, int high)
     }
 }
 
-int* array_with_random_numbers(int size, int min,int max){
+int* fill_array_with_data(int size){
     int* array = new int[size];
-    srand(time(NULL));
     for(int i = 0; i < size; i++){
-        array[i] = (rand() % (2*max+1)) + min;
+        array[i] = data_vector[i];
     }
     return array;
+}
+
+bool load_data(string file_name, int amount){
+    cout<<"Loading data from "<<file_name<<" file"<<endl;
+    ifstream fin;
+    fin.open(file_name,ios::in);
+    if(!fin.good()){
+        cout<<"Data file "<<file_name<<" not exist"<<endl;
+        fin.close();
+        return false;
+    }
+    string line;
+    int data_loaded = 0;
+    for(int i = 0; i < amount; i++){
+        fin>>line;
+        if(!fin.eof()){
+            data_vector.push_back(stoi(line));
+        }else{
+            cout<<"Not enough elements in data file "<<file_name<<endl;
+            return false;
+        } 
+        data_loaded++;
+    }
+    cout<<"Loaded correctly "<<data_loaded<<" numbers"<<endl;
+    fin.close();
+    return true;
 }
 
 bool load_config(){
@@ -62,17 +92,19 @@ bool load_config(){
     }
     vector<string> row;
     string line;
+    fin >> data_file_name >> data_amount;
+    fin >> results_file_name;
     while(!fin.eof()){
-        string sort_algorithm, size_of_structure, min_value, max_value, number_of_repeats;
-        fin>>sort_algorithm>>size_of_structure>>min_value>>max_value>>number_of_repeats;
-        if(sort_algorithm.size() == 0 || size_of_structure.size() == 0 || min_value.size() == 0 || max_value.size() == 0 || number_of_repeats.size() == 0){
+        string sort_algorithm, min_size, max_size, step, number_of_repeats;
+        fin>>sort_algorithm>>min_size>>max_size>>step>>number_of_repeats;
+        if(sort_algorithm.size() == 0 || min_size.size() == 0 || max_size.size() == 0 || step.size() == 0 || number_of_repeats.size() == 0){
             break;
         }
         vector<string> task;
         task.push_back(sort_algorithm);
-        task.push_back(size_of_structure);
-        task.push_back(min_value);
-        task.push_back(max_value);
+        task.push_back(min_size);
+        task.push_back(max_size);
+        task.push_back(step);
         task.push_back(number_of_repeats);
         tasks.push_back(task);
     }
@@ -83,35 +115,34 @@ bool load_config(){
 
 int main(){
     load_config();
+    if(!load_data(data_file_name,data_amount)){
+        cout<<"Cannot load "<<data_amount<<" numbers from "<<data_file_name<<" file."<<endl;
+    }
     if(tasks.size() == 0){
         cout<<"No tasks found to be performed."<<endl;
     }else{
         for(int i = 0;i < tasks.size(); i++){
             string sort_algorithm = tasks[i][0];
-            int size_of_structure = stoi(tasks[i][1]);
-            int min_value = stoi(tasks[i][2]);
-            int max_value = stoi(tasks[i][3]);
+            int min_size = stoi(tasks[i][1]);
+            int max_size = stoi(tasks[i][2]);
+            int step = stoi(tasks[i][3]);
             int number_of_repeats = stoi(tasks[i][4]);
-            cout<<"Sorting "<<sort_algorithm<<" array of "<<size_of_structure<<" elements in range from "<<min_value<<" to "<<max_value<<","<<number_of_repeats<<" times"<<endl;
-            if(size_of_structure<1){
+            cout<<"Sorting "<<sort_algorithm<<" arrays with numbers ranging from "<<min_size<<" to "<<max_size<<", repeated"<<number_of_repeats<<" times"<<endl;
+            if(min_size<1){
                 cout<<"Cannot execute task. The array must to have at least 1 element.";
-            }else if(min_value<INT_MIN){
-                cout<<"Cannot execute task. The minimum value is outside the range of an integer.";
-            }else if(max_value>INT_MAX){
-                cout<<"Cannot execute task. The maximum value is outside the range of an integer.";
             }else if(number_of_repeats<1){
                 cout<<"Cannot execute task. The minimum number of repetitions is 1.";
             }else{
                 if(sort_algorithm == "quick_sort"){
-                    for(int j = 1; i <= number_of_repeats; i++){
-                        cout<<"Generating an array with "<<size_of_structure<<" elements."<<endl;
-                        int* test_array = array_with_random_numbers(size_of_structure,min_value,max_value);
-                        cout<<"Array created. Sorting started."<<endl;
-                        quick_sort(test_array,0,(size_of_structure-1));
+                    for(int j = min_size; j <= max_size; j+=step){
+                        cout<<"Sorting an array with "<<j<<" elements. "<<number_of_repeats<<" repeats of task."<<endl;
+                        for(int k = 1; k <= number_of_repeats; k++){
+                            int* test_array = fill_array_with_data(j);
+                            quick_sort(test_array,0,(j-1));
+                        }
                         cout<<"Sorting finished."<<endl;
                     }
                 }else if(sort_algorithm == "counting_sort"){
-
                 }else{
                     cout<<"Cannot recognize "<<sort_algorithm<<" algorithm.";
                 }
